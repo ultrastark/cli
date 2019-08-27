2411fb4d4ae7184778481cf0a47a424f9958810b
rbalet <r.balet@ultrastark.ch>
2019-08-27 10:37:01 +0200

    enhance - newFolders : add folder

--- src/index.js
+++ src/index.js
@@ -30,6 +30,36 @@ const newFolders = [
     path: 'app/shared',
   },
   {
+    path: 'app/shared/layouts',
+  },
+  {
+    path: 'app/shared/components',
+  },
+  {
+    path: 'app/shared/fragments',
+  },
+  {
+    path: 'app/shared/pipes',
+  },
+  {
+    path: 'app/core',
+  },
+  {
+    path: 'app/core/guards',
+  },
+  {
+    path: 'app/core/directives',
+  },
+  {
+    path: 'app/core/services',
+  },
+  {
+    path: 'app/core/bases',
+  },
+  {
+    path: 'app/core/models',
+  },
+  {
     path: 'scss',
   },
   {
